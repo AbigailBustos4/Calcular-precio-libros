b2b6526844558dbf66357b73e57293133047541d
AbigailBustos4 <bustosabigail100@gmail.com>
2024-11-29 09:37:32 -0300

    aun no terminado

--- menu-completo.py
+++ menu-completo.py
@@ -9,51 +9,20 @@ while True:
 
     opcion = int(input("Elige una opcion del 1 al 5: "))
     if opcion == 1: #paso 1
-        #Pedir el nombre del libro (Nombre-escuela-grado)
-        nombre = input("¿Cómo se llama el libro? ")
-        simple_faz = int(input("¿Cuantas hojas simple ByN tiene? "))
-        doble_faz = int(input("¿Cuantas hojas doble ByN tiene? "))
-        color_simple = int(input("¿Cuantas hojas simple COLOR tiene? "))
-        color_doble = int(input("¿Cuantas hojas doble COLOR tiene? "))
-
-        if simple_faz >= 1 and doble_faz == 0 and color_doble == 0 and color_simple == 0:
-            tipo = 1
-        elif simple_faz >= 0 and doble_faz == 1 and color_doble == 0 and color_simple == 0:
-            tipo = 2
-        elif simple_faz >= 0 and doble_faz == 0 and color_doble == 0 and color_simple == 1:
-            tipo = 3
-        elif simple_faz >= 0 and doble_faz == 0 and color_doble == 1 and color_simple == 0:
-            tipo = 4
-        else:
-            tipo = 5
-#precio de las hojas
-        precio_hojas = simple_faz * 60 + doble_faz * 80 + color_doble * 230 + color_simple * 150
-#precio del anillado
-        cantidad_hojas = simple_faz + doble_faz + color_doble + color_simple
-        if cantidad_hojas <= 20:
-            precio_anillado = 800
-        elif cantidad_hojas <= 50:
-            precio_anillado = 970
-        elif cantidad_hojas <= 100:
-            precio_anillado = 1100
-        elif cantidad_hojas <= 150:
-            precio_anillado = 1400
-        else:
-            precio_anillado = 1800
-
-#precio total del libro
-        precio_libro = precio_anillado + precio_hojas
-
-#Creo mi diccionario con el libro y sus datos
-        libro = {
-            "Nombre": nombre,
-            "Tipo": tipo,
-            "Precio": precio_libro
-        }
-
-        lista.append(libro)
-
-        print("Libro agregado con exito")
+        from paso1 import agregar_libro
+        agregar_libro()
+        while True:
+            print("¿Qué deseas hacer ahora?")
+            print("1. Agregar otro libro")
+            print("2. Volver al menú")
+            opcion2 = int(input("elige una opción "))
+            if opcion2 == 2:
+                break
+            elif opcion2 == 1:
+                agregar_libro()
+            else:
+                print("Opcion no valida. Volviendo al menú")
+                break
     elif opcion == 2: 
         #Tengo que poder hacer consultas en toda la lista de libros
         pass
--- paso1.py
+++ paso1.py
@@ -14,49 +14,48 @@ Asigna 5 si es mixto
 """
 #la lista que contenga el diccionario
 lista = []
+def agregar_libro():
+    nombre = input("¿Cómo se llama el libro? ") #Pedir el nombre del libro (Nombre-escuela-grado)
+    simple_faz = int(input("¿Cuantas hojas simple ByN tiene? "))
+    doble_faz = int(input("¿Cuantas hojas doble ByN tiene? "))
+    color_simple = int(input("¿Cuantas hojas simple COLOR tiene? "))
+    color_doble = int(input("¿Cuantas hojas doble COLOR tiene? "))
 
-#Pedir el nombre del libro (Nombre-escuela-grado)
-nombre = input("¿Cómo se llama el libro? ")
-simple_faz = int(input("¿Cuantas hojas simple ByN tiene? "))
-doble_faz = int(input("¿Cuantas hojas doble ByN tiene? "))
-color_simple = int(input("¿Cuantas hojas simple COLOR tiene? "))
-color_doble = int(input("¿Cuantas hojas doble COLOR tiene? "))
+    if simple_faz >= 1 and doble_faz == 0 and color_doble == 0 and color_simple == 0:
+        tipo = 1
+    elif simple_faz >= 0 and doble_faz == 1 and color_doble == 0 and color_simple == 0:
+        tipo = 2
+    elif simple_faz >= 0 and doble_faz == 0 and color_doble == 0 and color_simple == 1:
+        tipo = 3
+    elif simple_faz >= 0 and doble_faz == 0 and color_doble == 1 and color_simple == 0:
+        tipo = 4
+    else:
+        tipo = 5
+    #precio de las hojas
+    precio_hojas = simple_faz * 60 + doble_faz * 80 + color_doble * 230 + color_simple * 150
+    #precio del anillado
+    cantidad_hojas = simple_faz + doble_faz + color_doble + color_simple
+    if cantidad_hojas <= 20:
+        precio_anillado = 800
+    elif cantidad_hojas <= 50:
+        precio_anillado = 970
+    elif cantidad_hojas <= 100:
+        precio_anillado = 1100
+    elif cantidad_hojas <= 150:
+        precio_anillado = 1400
+    else:
+        precio_anillado = 1800
 
-if simple_faz >= 1 and doble_faz == 0 and color_doble == 0 and color_simple == 0:
-    tipo = 1
-elif simple_faz >= 0 and doble_faz == 1 and color_doble == 0 and color_simple == 0:
-    tipo = 2
-elif simple_faz >= 0 and doble_faz == 0 and color_doble == 0 and color_simple == 1:
-    tipo = 3
-elif simple_faz >= 0 and doble_faz == 0 and color_doble == 1 and color_simple == 0:
-    tipo = 4
-else:
-    tipo = 5
-#precio de las hojas
-precio_hojas = simple_faz * 60 + doble_faz * 80 + color_doble * 230 + color_simple * 150
-#precio del anillado
-cantidad_hojas = simple_faz + doble_faz + color_doble + color_simple
-if cantidad_hojas <= 20:
-    precio_anillado = 800
-elif cantidad_hojas <= 50:
-    precio_anillado = 970
-elif cantidad_hojas <= 100:
-    precio_anillado = 1100
-elif cantidad_hojas <= 150:
-    precio_anillado = 1400
-else:
-    precio_anillado = 1800
+    #precio total del libro
+    precio_libro = precio_anillado + precio_hojas
 
-#precio total del libro
-precio_libro = precio_anillado + precio_hojas
+    #Creo mi diccionario con el libro y sus datos
+    libro = {
+        "Nombre": nombre,
+        "Tipo": tipo,
+        "Precio": precio_libro
+    }
 
-#Creo mi diccionario con el libro y sus datos
-libro = {
-    "Nombre": nombre,
-    "Tipo": tipo,
-    "Precio": precio_libro
-}
+    lista.append(libro)
 
-lista.append(libro)
-
-print("Libro agregado con exito")
+    print(f"Libro agregado con exito. El precio es ${precio_libro}")
